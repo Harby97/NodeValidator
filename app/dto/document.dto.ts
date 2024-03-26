cc17c764ae1b495a35f6e0493f10ab72174b9829
harby97 <ingharby@gmail.com>
2024-03-26 16:47:10 -0500

    validacion de documentos

---
+++ app/dto/document.dto.ts
@@ -0,0 +1,23 @@
+import { IsDefined, IsNotEmptyObject, IsObject, ValidateNested } from 'class-validator';
+import { IsOptional } from 'class-validator';
+import { IsInt, IsLowercase, IsNotEmpty, IsString, Matches, MaxLength } from 'class-validator';
+
+
+export class DocumentDTO {
+    @IsString({ message: 'el nombre del indice debe ser letras' })
+    @IsLowercase({ message: 'el nombre del indice debe estar en minúsculas' })
+    @Matches(/^(?![-_+])(?!.*\.\.?$)[^\\\/*?"<>|,`#]+$/, {
+        message: 'El nombre no puede incluir \\, /, *, ?, ", <, >, |, `, ,, # y no puede empezar con -, _, +, ni ser . o ..',
+    })
+    @MaxLength(250, { message: 'El nombre no puede tener más de 250 caracteres' })
+    @IsOptional({ message: 'el nombre es obligatorio' })
+    indexName!: string;
+    
+    @IsString({ message: 'el id es debe ser un string' })
+    @IsOptional({ message: 'el id es opcional' })
+    id!: string;
+
+    @IsObject({ message: 'body debe ser un objeto' })
+    @IsDefined({ message: 'body es obligatorio' })
+    body!: object;
+}
--- app/index.ts
+++ app/index.ts
@@ -1,14 +1,17 @@
 import 'reflect-metadata';
 import * as validator from '../app/middleware/index.middleware';
+import * as docVal from '../app/middleware/document.middleware';
 import bodyParser from 'body-parser';
 import express from "express";
 
 const app: any = express();
 app.use(bodyParser.json());
 const indexValidator = new validator.IndexMiddleware();
+const documentValidator = new docVal.DocumentMiddleware();
 
 
 app.post("/api/create", indexValidator.indexValidator);
+app.post("/api/document", documentValidator.documentValidator);
 app.listen(3000, () => {
     console.log("Escuchando en el puerto 3000 ");
 });
